d338c6e93b9926a6f094aa81654a79405adf2dbe
Alexander Fyrdahl <alexander.fyrdahl@gmail.com>
2017-10-30 10:34:10 +0100

    Update isFib.m

--- isFib.m
+++ isFib.m
@@ -1,13 +1,13 @@
 function [ out ] = isFib( in )
-out = false;
-x1 = 5 * in^2 + 4;
-x2 = 5 * in^2 - 4;
-if rem(sqrt(x1), 1) <= eps
-    out = true;
-end
-if rem(sqrt(x2), 1) <= eps
-    out = true;
-end
-
+    
+    x1 = 5 * in^2 + 4;
+    x2 = 5 * in^2 - 4;
+    
+    if (rem(sqrt(x1), 1) <= eps) || (rem(sqrt(x2), 1) <= eps)
+        out = true;
+    else
+        out = false;
+    end
+    
 end
 
